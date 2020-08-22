ac268bbf71c5d2949284d33c49e0c22c70f8f925
UhMarco <ddefygravity@gmail.com>
2020-08-22 19:18:19 +0200

    Actually fixed

--- src/Commands/Utilities/Ping.js
+++ src/Commands/Utilities/Ping.js
@@ -2,17 +2,18 @@ const Command = require('../../Structures/Command');
 
 module.exports = class extends Command {
 
-  constructor(...args) {
-    super(...args, {
-      aliases: ['pong']
-    });
-  }
+	constructor(...args) {
+		super(...args, {
+			aliases: ['pong']
+		});
+	}
 
-  async run(message) {
-    const msg = await message.channel.send('Pinging...');
+	async run(message) {
+		const msg = await message.channel.send('Pinging...');
 
-    const latency = msg.createdTimestamp - message.createdTimestamp;
+		const latency = msg.createdTimestamp - message.createdTimestamp;
 
-    msg.edit(`Pong! - Bot Latency \`${latency}ms\`, API Latency: \`${Math.round(this.client.ws.ping)}ms\``);
-  }
-}
+		msg.edit(`Pong! - Bot Latency \`${latency}ms\`, API Latency: \`${Math.round(this.client.ws.ping)}ms\``);
+	}
+
+};
--- src/Structures/Bot.js
+++ src/Structures/Bot.js
@@ -3,57 +3,61 @@ const Util = require('./Util.js');
 
 module.exports = class Bot extends Client {
 
-  constructor(options = {}) {
-    super({
-      disableMentions: 'everyone'
-    });
-    this.validate(options);
+	constructor(options = {}) {
+		super({
+			disableMentions: 'everyone'
+		});
+		this.validate(options);
 
-    this.commands = new Collection()
+		// eslint-disable-next-line semi
+		this.commands = new Collection()
 
-    this.aliases = new Collection()
+		// eslint-disable-next-line semi
+		this.aliases = new Collection()
 
-    this.utils = new Util(this)
+		// eslint-disable-next-line semi
+		this.utils = new Util(this)
 
-    this.once('ready', () => {
-      console.log(`Logged in as ${this.user.username}`);
-    });
+		this.once('ready', () => {
+			console.log(`Logged in as ${this.user.username}`);
+		});
 
-    this.on('message', async (message) => {
-      const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
-      const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
+		this.on('message', async (message) => {
+			const mentionRegex = RegExp(`^<@!${this.user.id}>$`);
+			const mentionRegexPrefix = RegExp(`^<@!${this.user.id}> `);
 
-      if(!message.guild || message.author.bot) return;
+			if (!message.guild || message.author.bot) return;
 
-      if(message.content.match(mentionRegex)) message.channel.send(`My prefix is \`${this.prefix}\``)
+			if (message.content.match(mentionRegex)) message.channel.send(`My prefix is \`${this.prefix}\``);
 
-      const prefix = message.content.match(mentionRegexPrefix) ?
-        message.content.match(mentionRegexPrefix)[0] : this.prefix;
+			const prefix = message.content.match(mentionRegexPrefix) ?
+				message.content.match(mentionRegexPrefix)[0] : this.prefix;
 
-      if(!message.content.startsWith(prefix)) return;
+			if (!message.content.startsWith(prefix)) return;
 
-      const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+			const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
 
-  		const command = this.commands.get(cmd.toLowerCase()) || this.commands.get(this.aliases.get(cmd.toLowerCase()));
-  		if (command) {
-  			command.run(message, args);
-  		}
-    });
-  }
+			const command = this.commands.get(cmd.toLowerCase()) || this.commands.get(this.aliases.get(cmd.toLowerCase()));
+			if (command) {
+				command.run(message, args);
+			}
+		});
+	}
 
-  validate(options) {
-    if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
+	validate(options) {
+		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
 
-    if (!options.token) throw new Error('You must pass the token for the client.');
-    this.token = options.token;
+		if (!options.token) throw new Error('You must pass the token for the client.');
+		this.token = options.token;
 
-    if (!options.prefix) throw new TypeError('You must pass the prefix for the client.');
-    if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
-    this.prefix = options.prefix;
-  }
+		if (!options.prefix) throw new TypeError('You must pass the prefix for the client.');
+		if (typeof options.prefix !== 'string') throw new TypeError('Prefix should be a type of String.');
+		this.prefix = options.prefix;
+	}
 
-  async start(token = this.token) {
-    this.utils.loadCommands();
-    super.login(token);
-  }
-}
+	async start(token = this.token) {
+		this.utils.loadCommands();
+		super.login(token);
+	}
+
+};
