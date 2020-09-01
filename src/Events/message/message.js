e41cd8433b0ddbd5b7e3f1271d37b29c246bc171
UhMarco <ddefygravity@gmail.com>
2020-09-01 16:47:53 +0100

    Violations do not work: almost there. Just realised I'm going to have to add a mute role - on.guild.join => set everything up if muted role doesn't exist

--- src/Events/message/message.js
+++ src/Events/message/message.js
@@ -10,10 +10,11 @@ module.exports = class extends Event {
 
     // FILTER
     const file = f.loadJSON(`${message.guild.id}.json`);
-    if (file && !message.member.hasPermission("ADMINISTRATOR")) {
+    if (file && message.member.hasPermission("ADMINISTRATOR")) {
       file.phrases.forEach(phrase => {
         if (message.content.toLowerCase().includes(phrase)) {
           message.delete();
+          f.violation(message);
           return message.channel.send(`<@${message.author.id}> - filtered word violation.`);
         }
       });
--- src/Structures/Functions.js
+++ src/Structures/Functions.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 module.exports = {
 
+  // JSON FUNCTIONS
+
   createJSON: function (message) {
     template = __dirname + `/../Data/template.json`;
     created = __dirname + `/../Data/${message.guild.id}.json`;
@@ -34,6 +36,51 @@ module.exports = {
         throw err;
       }
     })
+  },
+
+
+  // OTHER FUNCTIONS
+
+  violation: function (message) {
+
+    let file = module.exports.loadJSON(`${message.guild.id}.json`);
+
+    console.log("Running violation function.");
+
+    let history = file.history;
+    let limit = file.limit;
+
+    console.log(`History: ${history} / Limit: ${limit}`);
+
+    if (limit == 0) return;
+
+    var found = false;
+    var violations = 1;
+
+    for (var i = 0; i < history.length; i++) { // Check if user already has violation history...
+      if (history[i][0] == message.author.id) {
+        found = true;
+        violations = history[i][1]
+        violations++; // Increase history by one.
+        console.log("History found - increased by 1.");
+      }
+    }
+
+    if (!found) { // New offence.
+      history.push([message.author.id, violations]);
+      console.log("New offence.");
+    }
+
+    if (violations > limit) {
+      // Punish
+      console.log("Punishing and resetting violations.");
+      violations = 0;
+    }
+
+    // Finally update file.
+    module.exports = editJSON(message, file);
+    console.log("File updated.");
+
   }
 
 }
