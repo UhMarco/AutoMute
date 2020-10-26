05e520ca6146da5237e3bbe0b74f86a0f331e85b
UhMarco <ddefygravity@gmail.com>
2020-10-26 13:31:03 +0000

    Removed debugging logs

--- src/Structures/Functions.js
+++ src/Structures/Functions.js
@@ -45,8 +45,6 @@ module.exports = {
 
     let file = module.exports.loadJSON(`${message.guild.id}.json`);
 
-    console.log("Running violation function.");
-
     if (file.limit == 0) return false;
 
     let found = false;
@@ -56,27 +54,22 @@ module.exports = {
       if (file.history[i][0] == message.author.id) {
         found = true;
         file.history[i][1]++; // Increase history by one.
-        console.log("History found - increased by 1.");
 
         if (file.history[i][1] >= file.limit) {
           // Punish
-          console.log("Punishing and resetting violations.");
           file.history.pop(i)
           punish = true;
         }
-
       }
     }
 
     if (!found) { // New offence.
-      console.log("New offence.");
       if (file.limit == 1) return true;
       file.history.push([message.author.id, 1]);
     }
 
     // Finally update file.
     module.exports.editJSON(message, file);
-    console.log("File updated.");
     return punish;
   }
 
